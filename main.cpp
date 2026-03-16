9f8283079d6fb738052418b7afb2071010734793
qwyzex <qwyzex@gmail.com>
2026-03-16 08:34:23 +0700

    ✨ feat(visualization): add interactive 3d mode and unify view controls - introduce an interactive 3D visualization mode with free camera controls - allow switching between 2D slice view and 3D block view using the TAB key - refactor 2D and 3D view logic into a single `castWindow` function - integrate raymath and rcamera for enhanced camera and math capabilities - enhance grid drawing with a custom colored grid function - update initial `x` and `y` dimensions for grid generation
    📦 build(.vscode): configure vs code for development
    - add chrome debugger attachment configuration
    - define a default build task to compile and run the project
    - update debug program path in launch.json to `main`

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include "raylib.h"
+#include "raymath.h"
+#define CAMERA_IMPLEMENTATION
+#include "rcamera.h"
 #include "rlgl.h"
 #include <cstdlib>
 #include <fstream>
@@ -9,7 +12,8 @@
 
 using namespace std;
 
-int x = 60, y = 60, z = 60;
+int x = 40, y = 50, z = 60;
+// int x = 40, y = 40, z = 40;
 int baseCoef = 4, neighborIntensity = 10, vacancyImpact = 3;
 int influence = 3;
 
@@ -164,84 +168,38 @@ void exportOre(const vector<vector<vector<block>>> &arr) {
   }
 }
 
-void castWindow3D(const vector<vector<vector<block>>> &arr) {
-  SetConfigFlags(FLAG_VSYNC_HINT);
-  SetConfigFlags(FLAG_MSAA_4X_HINT);
-  InitWindow(1000, 800, "Viviore 0.0.1");
-  SetTargetFPS(30);
-
-  Camera3D camera = {0};
-  camera.position = {70.0f, 70.0f, 70.0f};
-  camera.target = {0.0f, 0.0f, 0.0f};
-  camera.up = {0.0f, 1.0f, 0.0f};
-  //   camera.fovy = 45.0f;
-  camera.fovy = 60.0f;
-  camera.projection = CAMERA_PERSPECTIVE;
-
-  while (!WindowShouldClose()) {
-    UpdateCamera(&camera, CAMERA_FREE);
-
-    BeginDrawing();
-    ClearBackground(RAYWHITE);
-
-    BeginMode3D(camera);
-    BeginBlendMode(BLEND_ALPHA);
-
-    if (IsMouseButtonPressed(MOUSE_RIGHT_BUTTON))
-      DisableCursor();
-
-    if (IsMouseButtonReleased(MOUSE_RIGHT_BUTTON))
-      EnableCursor();
-    DrawGrid(100, 1.0f);
-    for (int i = 0; i < x; i++)
-      for (int j = 0; j < y; j++)
-        for (int k = 0; k < z; k++) {
-          if (arr[i][j][k].isOre) {
-            float cx = x / 2.0f;
-            float cy = y / 2.0f;
-            float cz = z / 2.0f;
-            Color oreColor;
-
-            double g = arr[i][j][k].grade;
-
-            if (g < 0.5)
-              oreColor = Fade(YELLOW, 0.6f);
-            else if (g < 0.8)
-              oreColor = Fade(ORANGE, 0.6f);
-            else
-              oreColor = Fade(RED, 0.6f);
-            // if (g < 0.5)
-            //   oreColor = YELLOW;
-            // else if (g < 0.8)
-            //   oreColor = ORANGE;
-            // else
-            //   oreColor = RED;
-
-            DrawCube({(float)i - cx, (float)j - cy, (float)k - cz}, 1.0f, 1.0f,
-                     1.0f, oreColor);
-            DrawCubeWires({(float)i - cx, (float)j - cy, (float)k - cz}, 1.0f,
-                          1.0f, 1.0f, BLACK);
-          }
-        }
-
-    EndMode3D();
-    EndDrawing();
-  }
-  CloseWindow();
+int AxisSize(Axis axis) {
+  if (axis == X)
+    return x;
+  if (axis == Y)
+    return y;
+  return z;
 };
 
-void castWindow2D(const vector<vector<vector<block>>> &arr, Axis &axis,
-                  int &slice) {
+void DrawColoredGrid(int slices, float spacing, Color color) {
+  float halfSlices = (float)slices / 2.0f;
+  float step = spacing;
+  float max = halfSlices * spacing;
+
+  for (float i = -max; i <= max; i += step) {
+    DrawLine3D((Vector3){i, 0.0f, -max}, (Vector3){i, 0.0f, max}, color);
+    DrawLine3D((Vector3){-max, 0.0f, i}, (Vector3){max, 0.0f, i}, color);
+  }
+}
+
+void castWindow(const vector<vector<vector<block>>> &arr, Axis &axis) {
   SetConfigFlags(FLAG_VSYNC_HINT);
   SetConfigFlags(FLAG_MSAA_4X_HINT);
   InitWindow(1200, 900, "Viviore 0.0.1");
   SetTargetFPS(30);
 
-  Camera camera = {0};
+  bool mode3D = false;
+
+  Camera3D camera = {0};
   camera.position = {70.0f, 70.0f, 70.0f};
   camera.target = {0.0f, 0.0f, 0.0f};
   camera.up = {0.0f, 1.0f, 0.0f};
-  camera.fovy = 45.0f;
+  camera.fovy = mode3D ? 60.0f : 45.0f;
   camera.projection = CAMERA_PERSPECTIVE;
 
   Camera3D miniCam = {0};
@@ -251,116 +209,253 @@ void castWindow2D(const vector<vector<vector<block>>> &arr, Axis &axis,
   miniCam.fovy = 45.0f;
   miniCam.projection = CAMERA_PERSPECTIVE;
 
+  int slice = 0;
+
+  int maxSlice;
+
   while (!WindowShouldClose()) {
     BeginDrawing();
-    ClearBackground(RAYWHITE);
+    ClearBackground(mode3D ? BLACK : RAYWHITE);
 
-    int maxSlice = axis == X ? x : axis == Y ? y : z;
+    if (IsKeyPressed(KEY_TAB))
+      mode3D = !mode3D;
 
-    // Rectangle viewport = {20, GetScreenHeight() - 180, 160, 160};
-    Rectangle viewport = {920, 600, 240, 180};
+    if (mode3D) {
+      // UpdateCamera(&camera, CAMERA_FREE);
 
-    rlViewport(viewport.x, viewport.y, viewport.width, viewport.height);
+      BeginMode3D(camera);
+      BeginBlendMode(BLEND_ALPHA);
 
-    BeginMode3D(miniCam);
-
-    DrawCubeWires({0, 0, 0}, 4, 4, 4, DARKGRAY);
-
-    // float px = (float)slice / (x - 1) * 4 - 1;
-    // float py = (float)slice / (y - 1) * 4 - 1;
-    // float pz = (float)slice / (z - 1) * 4 - 1;
-
-    float p = (float)slice / (maxSlice - 1) * 4 - 2;
-
-    if (axis == X)
-      DrawCube({p, 0, 0}, 0.05f, 4, 4, Fade(RED, 0.5f));
-    if (axis == Y)
-      DrawCube({0, p, 0}, 4, 0.05f, 4, Fade(RED, 0.5f));
-    if (axis == Z)
-      DrawCube({0, 0, p}, 4, 4, 0.05f, Fade(RED, 0.5f));
-
-    DrawLine3D({0, 0, 0}, {2.9, 0, 0}, RED);
-    DrawLine3D({0, 0, 0}, {0, 2.9, 0}, GREEN);
-    DrawLine3D({0, 0, 0}, {0, 0, 2.9}, BLUE);
-
-    EndMode3D();
-
-    // restore normal rendering
-    rlViewport(0, 0, GetScreenWidth(), GetScreenHeight());
-    int width, height;
-
-    switch (axis) {
-    case X:
-      width = y;
-      height = z;
-      break;
-    case Y:
-      width = x;
-      height = z;
-      break;
-    case Z:
-      width = x;
-      height = y;
-      break;
-    }
-
-    float cellSize = min(900.0f / width, 900.0f / height);
-
-    if (IsKeyPressed(KEY_RIGHT))
-      slice = min(slice + 1, maxSlice - 1);
-    if (IsKeyPressed(KEY_LEFT))
-      slice = max(slice - 1, 0);
-
-    if (IsKeyPressed(KEY_UP))
-      axis = (Axis)((axis % 3) + 1);
-    if (IsKeyPressed(KEY_DOWN))
-      axis = (Axis)((axis + 1) % 3 + 1);
-
-    string axisName = axis == X   ? "X (Left-Right)"
-                      : axis == Y ? "Y (Depth)"
-                                  : "Z (Front-Back)";
-
-    DrawText(TextFormat("Axis: %s", axisName.c_str()), 920, 20, 20, BLACK);
-    DrawText(TextFormat("Slice: %d / %d", slice, maxSlice - 1), 920, 50, 20,
-             BLACK);
-
-    for (int i = 0; i < width; i++) {
-      for (int j = 0; j < height; j++) {
-        block item;
-
-        switch (axis) {
-        case X:
-          item = arr[slice][i][j];
-          break;
-        case Y:
-          item = arr[i][slice][j];
-          break;
-        case Z:
-          item = arr[i][j][slice];
-          break;
-        }
-
-        Color c = LIGHTGRAY;
-
-        if (item.isOre) {
-          if (item.grade < 0.5)
-            c = YELLOW;
-          else if (item.grade < 0.8)
-            c = ORANGE;
-          else
-            c = RED;
-        }
-
-        DrawRectangleLines(i * cellSize, j * cellSize, cellSize, cellSize,
-                           DARKGRAY);
-        DrawRectangle(i * cellSize, j * cellSize, cellSize, cellSize, c);
+      if (IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)) {
+        DisableCursor();
       }
-    }
 
-    EndDrawing();
+      if (IsMouseButtonDown(MOUSE_RIGHT_BUTTON)) {
+        UpdateCamera(&camera, CAMERA_FREE);
+        DisableCursor();
+
+        Vector2 delta = GetMouseDelta();
+        float sensitivity = 0.003f;
+
+        Vector3 rotation = {-delta.y * sensitivity, -delta.x * sensitivity, 0};
+
+        Matrix rot = MatrixRotateXYZ(rotation);
+
+        Vector3 dir = Vector3Subtract(camera.target, camera.position);
+        dir = Vector3Transform(dir, rot);
+
+        camera.target = Vector3Add(camera.position, dir);
+      }
+
+      if (IsMouseButtonReleased(MOUSE_RIGHT_BUTTON)) {
+        EnableCursor();
+        GetMouseDelta();
+      }
+
+      if (IsMouseButtonDown(MOUSE_MIDDLE_BUTTON)) {
+        Vector2 delta = GetMouseDelta();
+
+        camera.target.x -= delta.x * 0.05f;
+        camera.target.y += delta.y * 0.05f;
+
+        camera.position.x -= delta.x * 0.05f;
+        camera.position.y += delta.y * 0.05f;
+      }
+
+      float speed = 0.5f;
+
+      Vector3 forward =
+          Vector3Normalize(Vector3Subtract(camera.target, camera.position));
+      Vector3 right = Vector3Normalize(Vector3CrossProduct(forward, camera.up));
+
+      forward.y = 0;
+      forward = Vector3Normalize(forward);
+
+      if (IsKeyDown(KEY_W)) {
+        camera.position =
+            Vector3Add(camera.position, Vector3Scale(forward, speed));
+        camera.target = Vector3Add(camera.target, Vector3Scale(forward, speed));
+      }
+
+      if (IsKeyDown(KEY_S)) {
+        camera.position =
+            Vector3Subtract(camera.position, Vector3Scale(forward, speed));
+        camera.target =
+            Vector3Subtract(camera.target, Vector3Scale(forward, speed));
+      }
+
+      if (IsKeyDown(KEY_A)) {
+        camera.position =
+            Vector3Subtract(camera.position, Vector3Scale(right, speed));
+        camera.target =
+            Vector3Subtract(camera.target, Vector3Scale(right, speed));
+      }
+
+      if (IsKeyDown(KEY_D)) {
+        camera.position =
+            Vector3Add(camera.position, Vector3Scale(right, speed));
+        camera.target = Vector3Add(camera.target, Vector3Scale(right, speed));
+      }
+
+      float wheel = GetMouseWheelMove();
+      camera.position.x +=
+          wheel * (camera.target.x - camera.position.x) * 0.05f;
+      camera.position.y +=
+          wheel * (camera.target.y - camera.position.y) * 0.05f;
+      camera.position.z +=
+          wheel * (camera.target.z - camera.position.z) * 0.05f;
+
+      DrawColoredGrid(100, 1.0f, PURPLE);
+
+      for (int i = 0; i < x; i++)
+        for (int j = 0; j < y; j++)
+          for (int k = 0; k < z; k++) {
+            if (arr[i][j][k].isOre) {
+              float cx = x / 2.0f;
+              float cy = y / 2.0f;
+              float cz = z / 2.0f;
+              Color oreColor;
+
+              double g = arr[i][j][k].grade;
+
+              if (g < 0.5)
+                oreColor = Fade(YELLOW, 0.6f);
+              else if (g < 0.8)
+                oreColor = Fade(ORANGE, 0.6f);
+              else
+                oreColor = Fade(RED, 0.6f);
+              // if (g < 0.5)
+              //   oreColor = YELLOW;
+              // else if (g < 0.8)
+              //   oreColor = ORANGE;
+              // else
+              //   oreColor = RED;
+
+              DrawCube({(float)i - cx, (float)j - cy, (float)k - cz}, 1.0f,
+                       1.0f, 1.0f, oreColor);
+              DrawCubeWires({(float)i - cx, (float)j - cy, (float)k - cz}, 1.0f,
+                            1.0f, 1.0f, RAYWHITE);
+            }
+          }
+
+      EndMode3D();
+      EndDrawing();
+    } else {
+      // KEYBINDS
+      if (IsKeyPressed(KEY_RIGHT))
+        slice = min(slice + 1, maxSlice - 1);
+      if (IsKeyPressed(KEY_LEFT))
+        slice = max(slice - 1, 0);
+
+      if (IsKeyPressed(KEY_UP))
+        axis = (Axis)((axis % 3) + 1);
+      if (IsKeyPressed(KEY_DOWN))
+        axis = (Axis)((axis + 1) % 3 + 1);
+
+      // PRIMITIVES
+      int width, height;
+
+      switch (axis) {
+      case X:
+        width = y;
+        height = z;
+        break;
+      case Y:
+        width = x;
+        height = z;
+        break;
+      case Z:
+        width = x;
+        height = y;
+        break;
+      }
+
+      float cellSize = min(900.0f / width, 900.0f / height);
+
+      switch (axis) {
+      case X:
+        maxSlice = x;
+        break;
+      case Y:
+        maxSlice = y;
+        break;
+      case Z:
+        maxSlice = z;
+        break;
+      }
+
+      slice = std::max(0, std::min(slice, maxSlice - 1));
+
+      // 3D CUBE SLICE OVERVIEW
+      Rectangle viewport = {920, 620, 240, 180};
+      rlViewport(viewport.x, viewport.y, viewport.width, viewport.height);
+
+      BeginMode3D(miniCam);
+      DrawCubeWires({0, 0, 0}, 4, 4, 4, DARKGRAY);
+      float p = (maxSlice > 1) ? (float)slice / (maxSlice - 1) * 4 - 2 : 0;
+
+      if (axis == X)
+        DrawCube({p, 0, 0}, 0.05f, 4, 4, Fade(RED, 0.5f));
+      if (axis == Y)
+        DrawCube({0, p, 0}, 4, 0.05f, 4, Fade(RED, 0.5f));
+      if (axis == Z)
+        DrawCube({0, 0, p}, 4, 4, 0.05f, Fade(RED, 0.5f));
+
+      DrawLine3D({0, 0, 0}, {2.9, 0, 0}, RED);
+      DrawLine3D({0, 0, 0}, {0, 2.9, 0}, GREEN);
+      DrawLine3D({0, 0, 0}, {0, 0, 2.9}, BLUE);
+
+      EndMode3D();
+      rlViewport(0, 0, GetScreenWidth(), GetScreenHeight());
+
+      // OVERLAY
+      string axisName = axis == X   ? "X (Left-Right)"
+                        : axis == Y ? "Y (Depth)"
+                                    : "Z (Front-Back)";
+
+      DrawText(TextFormat("Axis: %s", axisName.c_str()), 920, 20, 20, BLACK);
+      DrawText(TextFormat("Slice: %d / %d", slice, maxSlice - 1), 920, 50, 20,
+               BLACK);
+
+      for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+          block item;
+
+          switch (axis) {
+          case X:
+            item = arr[slice][i][j];
+            break;
+          case Y:
+            item = arr[i][slice][j];
+            break;
+          case Z:
+            item = arr[i][j][slice];
+            break;
+          }
+
+          Color c = LIGHTGRAY;
+
+          if (item.isOre) {
+            if (item.grade < 0.5)
+              c = YELLOW;
+            else if (item.grade < 0.8)
+              c = ORANGE;
+            else
+              c = RED;
+          }
+
+          DrawRectangleLines(i * cellSize, j * cellSize, cellSize, cellSize,
+                             DARKGRAY);
+          DrawRectangle(i * cellSize, j * cellSize, cellSize, cellSize, c);
+        }
+      }
+
+      EndDrawing();
+    }
   }
+
   CloseWindow();
-}
+};
 
 int main() {
   srand(time(NULL));
@@ -376,7 +471,6 @@ int main() {
   cout << "Neighbouring Intensity: " << neighborIntensity << "%" << endl;
 
   Axis currentAxis = Y;
-  int slice = 0;
 
   plantOres(litho);
   seedGrades(litho);
@@ -384,7 +478,7 @@ int main() {
 
   exportOre(litho);
   // castWindow3D(litho);
-  castWindow2D(litho, currentAxis, slice);
+  castWindow(litho, currentAxis);
 
   return 0;
 }
